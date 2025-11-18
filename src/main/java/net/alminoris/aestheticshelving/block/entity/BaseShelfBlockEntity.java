70f4791d0b91c988e85933d87279e012a4861cbc
AOA <aoa@d.co>
2025-11-18 19:56:28 +0200

    #Update 2.0.0

--- src/main/java/net/alminoris/aestheticshelving/block/entity/BaseShelfBlockEntity.java
+++ src/main/java/net/alminoris/aestheticshelving/block/entity/BaseShelfBlockEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.MenuProvider;
@@ -112,7 +113,7 @@ public class BaseShelfBlockEntity extends BlockEntity implements MenuProvider, I
     @Override
     public Component getDisplayName()
     {
-        return Component.translatable("block.aestheticshelving." + name);
+        return new TranslatableComponent("block.aestheticshelving." + name);
     }
 
     @Nullable
